e7c4efeda13ca5476a54a1a74d6179cf6e1bd691
godspell2014 <anthony.kirkland@gmail.com>
2014-03-06 15:59:23 -0600

    .

---
+++ act2_songs.js
@@ -0,0 +1,8 @@
+var songs=new Array();
+songs[0]= {name: "Turn Back O Man", start: (2*60 + 47)}
+songs[1]= {name: "Alas for You", start: (11*60 + 2)}
+songs[2] = {name: "By My Side", start: (19*60 + 44)}
+songs[3] = {name: "We Beseech Thee", start: (28*60 + 22)}
+songs[4] = {name: "Beautiful City", start: (31*60 + 40)}
+songs[5] = {name: "On the Willows", start: (37*60 + 20)}
+songs[6] = {name: "Finale", start: (43*60 + 55)}
